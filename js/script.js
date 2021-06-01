2fb6f93aea9073a057a0e539a5f5915c540b34af
vvalcarcell <valcarcellvalentina@gmail.com>
2021-06-01 18:01:43 +0200

    output

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
+var output = document.getElementById('num');
 for (var x = 1; x <= 100; x++) {
     if (x % 3 === 0 && x % 5 !== 0) {
-        document.getElementById('num').innerHTML += " " + "Fizz";
+        output.innerHTML += " " + "Fizz";
     } else if (x % 5 === 0 && x % 3 !== 0) {
-        document.getElementById('num').innerHTML += " " + "Buzz";
+        output.innerHTML += " " + "Buzz";
     } else if (x % 3 === 0 && x % 5 === 0) {
-        document.getElementById('num').innerHTML += " " + "FizzBuzz";
+        output.innerHTML += " " + "FizzBuzz";
     } else {
-        document.getElementById('num').innerHTML += " " + x;
+        output.innerHTML += " " + x;
     }
 }
